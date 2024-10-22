9959e617bf36a3dad6addefe48c9c5ba3999d9f0
LaneAzur <147050708+LaneAzur@users.noreply.github.com>
2024-10-22 15:40:58 +0800

    Add files via upload

---
+++ utils/losses.py
@@ -0,0 +1,171 @@
+import torch
+import torch.nn.functional as F
+import numpy as np
+import math
+
+def smoothloss(y_pred):
+    d2, h2, w2 = y_pred.shape[-3:]
+    dy = torch.abs(y_pred[:,:,1:, :, :] - y_pred[:,:, :-1, :, :]) / 2 * d2
+    dx = torch.abs(y_pred[:,:,:, 1:, :] - y_pred[:,:, :, :-1, :]) / 2 * h2
+    dz = torch.abs(y_pred[:,:,:, :, 1:] - y_pred[:,:, :, :, :-1]) / 2 * w2
+    
+    return (torch.mean(dx * dx) + torch.mean(dy * dy) + torch.mean(dz * dz)) / 3.0
+
+class Grad3d(torch.nn.Module):
+    """
+    N-D gradient loss.
+    """
+
+    def __init__(self, penalty='l1', loss_mult=None):
+        super(Grad3d, self).__init__()
+        self.penalty = penalty
+        self.loss_mult = loss_mult
+
+    def forward(self, y_pred, y_true):
+        dy = torch.abs(y_pred[:, :, 1:, :, :] - y_pred[:, :, :-1, :, :])
+        dx = torch.abs(y_pred[:, :, :, 1:, :] - y_pred[:, :, :, :-1, :])
+        dz = torch.abs(y_pred[:, :, :, :, 1:] - y_pred[:, :, :, :, :-1])
+
+        if self.penalty == 'l2':
+            dy = dy * dy
+            dx = dx * dx
+            dz = dz * dz
+
+        d = torch.mean(dx) + torch.mean(dy) + torch.mean(dz)
+        grad = d / 3.0
+
+        if self.loss_mult is not None:
+            grad *= self.loss_mult
+        return grad
+
+
+class NCC_vxm(torch.nn.Module):
+    """
+    Local (over window) normalized cross correlation loss.
+    """
+
+    def __init__(self, win=None):
+        super(NCC_vxm, self).__init__()
+        self.win = win
+
+    def forward(self, y_true, y_pred):
+
+        Ii = y_true
+        Ji = y_pred
+
+        # get dimension of volume
+        # assumes Ii, Ji are sized [batch_size, *vol_shape, nb_feats]
+        ndims = len(list(Ii.size())) - 2
+        assert ndims in [1, 2, 3], "volumes should be 1 to 3 dimensions. found: %d" % ndims
+
+        # set window size
+        win = [9] * ndims if self.win is None else self.win
+
+        # compute filters
+        sum_filt = torch.ones([1, 1, *win]).to("cuda")
+
+        pad_no = math.floor(win[0] / 2)
+
+        if ndims == 1:
+            stride = (1)
+            padding = (pad_no)
+        elif ndims == 2:
+            stride = (1, 1)
+            padding = (pad_no, pad_no)
+        else:
+            stride = (1, 1, 1)
+            padding = (pad_no, pad_no, pad_no)
+
+        # get convolution function
+        conv_fn = getattr(F, 'conv%dd' % ndims)
+
+        # compute CC squares
+        I2 = Ii * Ii
+        J2 = Ji * Ji
+        IJ = Ii * Ji
+
+        I_sum = conv_fn(Ii, sum_filt, stride=stride, padding=padding)
+        J_sum = conv_fn(Ji, sum_filt, stride=stride, padding=padding)
+        I2_sum = conv_fn(I2, sum_filt, stride=stride, padding=padding)
+        J2_sum = conv_fn(J2, sum_filt, stride=stride, padding=padding)
+        IJ_sum = conv_fn(IJ, sum_filt, stride=stride, padding=padding)
+
+        win_size = np.prod(win)
+        u_I = I_sum / win_size
+        u_J = J_sum / win_size
+
+        cross = IJ_sum - u_J * I_sum - u_I * J_sum + u_I * u_J * win_size
+        I_var = I2_sum - 2 * u_I * I_sum + u_I * u_I * win_size
+        J_var = J2_sum - 2 * u_J * J_sum + u_J * u_J * win_size
+
+        cc = cross * cross / (I_var * J_var + 1e-5)
+
+        return -torch.mean(cc)
+
+
+def DSC(pred, target):
+    smooth = 1e-5
+    m1 = pred.flatten()
+    m2 = target.flatten()
+    intersection = (m1 * m2).sum()
+    return (2. * intersection + smooth) / (m1.sum() + m2.sum() + smooth)
+
+class contrastive_loss(torch.nn.Module):
+
+    def __init__(self, batch_size=2, temperature=0.5, use_cosine_similarity=True):
+        super(contrastive_loss, self).__init__()
+        self.batch_size = batch_size
+        self.temperature = temperature
+        self.softmax = torch.nn.Softmax(dim=-1)
+        self.mask_samples_from_same_repr = self._get_correlated_mask().type(torch.bool)
+        self.similarity_function = self._get_similarity_function(use_cosine_similarity)
+        self.criterion = torch.nn.CrossEntropyLoss(reduction="sum")
+
+    def _get_similarity_function(self, use_cosine_similarity):
+        if use_cosine_similarity:
+            self._cosine_similarity = torch.nn.CosineSimilarity(dim=-1)
+            return self._cosine_simililarity
+        else:
+            return self._dot_simililarity
+
+    def _get_correlated_mask(self):
+        diag = np.eye(2 * self.batch_size)
+        l1 = np.eye((2 * self.batch_size), 2 * self.batch_size, k=-self.batch_size)
+        l2 = np.eye((2 * self.batch_size), 2 * self.batch_size, k=self.batch_size)
+        mask = torch.from_numpy((diag + l1 + l2))
+        mask = (1 - mask).type(torch.bool)
+        return mask.cuda()
+
+    @staticmethod
+    def _dot_simililarity(x, y):
+        v = torch.tensordot(x.unsqueeze(1), y.T.unsqueeze(0), dims=2)
+        # x shape: (N, 1, C)
+        # y shape: (1, C, 2N)
+        # v shape: (N, 2N)
+        return v
+
+    def _cosine_simililarity(self, x, y):
+        # x shape: (N, 1, C)
+        # y shape: (1, 2N, C)
+        # v shape: (N, 2N)
+        v = self._cosine_similarity(x.unsqueeze(1), y.unsqueeze(0))
+        return v
+
+    def __call__(self, zis, zjs):
+        # print(1, zjs.shape)
+
+        representations = torch.cat([zjs, zis], dim=0)
+        similarity_matrix = self.similarity_function(representations, representations)
+        # filter out the scores from the positive samples
+        l_pos = torch.diag(similarity_matrix, self.batch_size)
+        r_pos = torch.diag(similarity_matrix, -self.batch_size)
+        positives = torch.cat([l_pos, r_pos]).view(2 * self.batch_size, 1)
+        negatives = similarity_matrix[self.mask_samples_from_same_repr].view(2 * self.batch_size, -1)
+        logits = torch.cat((positives, negatives), dim=1)
+        logits /= self.temperature
+        # print('logits:', logits)
+
+        labels = torch.zeros(2 * self.batch_size).cuda().long()
+        loss = self.criterion(logits, labels)
+
+        return loss / (2 * self.batch_size)
